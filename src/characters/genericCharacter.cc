bb7e715f7d23600f2279c005213aaa71d0c29e33
Alex Kelly <alex.page.kelly@gmail.com>
2012-09-27 23:23:47 -0700

    Removed this, works except loads no files

--- src/characters/genericCharacter.cc
+++
@@ -1,7 +0,0 @@
-#include "name.h"
-name::name(){
-	head = new actionTrie;
-	airHead = new actionTrie;
-	meter = new int[3];
-	build("Name", "Name");
-}
--- src/characters/genericCharacter.h
+++
@@ -1,8 +0,0 @@
-#ifndef GREY_INCLUDED
-#define GREY_INCLUDED
-#include "(LAST CHARACTER).h"
-class name : public character {
-public:
-name();
-};
-#endif
